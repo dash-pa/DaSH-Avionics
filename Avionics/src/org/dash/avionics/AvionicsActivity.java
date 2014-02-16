6d415dc6eba1187e3b7015f612f3aa716fc8782a
Rodrigo Damazio <none@none>
2014-02-15 22:20:41 -0200

    First few metrics laid out.

--- Avionics/src/org/dash/avionics/AvionicsActivity.java
+++ Avionics/src/org/dash/avionics/AvionicsActivity.java
@@ -1,15 +1,21 @@
 package org.dash.avionics;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.ViewById;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 @Fullscreen
 @EActivity(R.layout.activity_avionics)
 public class AvionicsActivity extends Activity {
+
+	@ViewById
+	TextView rpmView, powerView, heartView, headingView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +24,19 @@ public class AvionicsActivity extends Activity {
 		getActionBar().hide();
 	}
 
+	@AfterViews
+	protected void setValues() {
+		heartView.setText("123");
+	}
+
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
-	        super.onWindowFocusChanged(hasFocus);
-	        View decorView = getWindow().getDecorView();
-	       if (hasFocus) {
-	    	   // TODO: Support older versions
-	        decorView.setSystemUiVisibility(
-	                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-	        }
+		super.onWindowFocusChanged(hasFocus);
+		View decorView = getWindow().getDecorView();
+		if (hasFocus) {
+			// TODO: Support older versions
+			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
 	}
 }
